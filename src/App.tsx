1a043d002e6104a03c1400c90a4467e7d8ede75c
Ragib Ali <66816874+ragibalikhan@users.noreply.github.com>
2025-10-06 14:01:37 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ import Auth from "./pages/Auth";
 import Marketing from "./pages/Marketing";
 import Blog from "./pages/Blog";
 import BlogPost from "./pages/BlogPost";
+import ThankYou from './pages/ThankYou';
 
 const queryClient = new QueryClient();
 
@@ -42,6 +43,7 @@ const App = () => (
           <Route path="/careers" element={<Careers />} />
           <Route path="/contact-us" element={<Contact />} />
           <Route path="/marketing" element={<Marketing />} />
+          <Route path= "/thank-you" element= {<ThankYou />} />
           <Route path="/admin" element={<Admin />} />
           <Route path="/auth" element={<Auth />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
