0fd80e555c76bfc5545adca599aa863f76b23e19
ashutosh <ashutoshsahni833@gmail.com>
2026-01-12 19:53:55 +0530

    scroll_fix

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,8 @@ import BlogPost from "./pages/BlogPost";
 import Growth from "./pages/growth";
 import Hackathon from "./pages/Hackathon";
 
+import ScrollToTop from "./components/ScrollToTop";
+
 const queryClient = new QueryClient();
 
 const App = () => (
@@ -30,28 +32,29 @@ const App = () => (
         <Toaster />
         <Sonner />
         <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/about-us" element={<About />} />
-          <Route path="/work" element={<Work />} />
-          <Route path="/solutions" element={<Solutions />} />
-          <Route path="/solutions/*" element={<Solutions />} />
-          <Route path="/resources" element={<Resources />} />
-          <Route path="/blogs" element={<Blog />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/blog/:slug" element={<BlogPost />} />
-          <Route path="/project/:slug" element={<ProjectDetail />} />
-          <Route path="/careers" element={<Careers />} />
-          <Route path="/contact-us" element={<Contact />} />
-          <Route path="/marketing" element={<Marketing />} />
-          <Route path="/build-with-inwoix" element={<Growth />} />
-          <Route path="/CodetoCareer" element={<Hackathon />} />
-          <Route path="/admin" element={<Admin />} />
-          <Route path="/auth" element={<Auth />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-        </BrowserRouter> 
+          <ScrollToTop />
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/about-us" element={<About />} />
+            <Route path="/work" element={<Work />} />
+            <Route path="/solutions" element={<Solutions />} />
+            <Route path="/solutions/*" element={<Solutions />} />
+            <Route path="/resources" element={<Resources />} />
+            <Route path="/blogs" element={<Blog />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/blog/:slug" element={<BlogPost />} />
+            <Route path="/project/:slug" element={<ProjectDetail />} />
+            <Route path="/careers" element={<Careers />} />
+            <Route path="/contact-us" element={<Contact />} />
+            <Route path="/marketing" element={<Marketing />} />
+            <Route path="/build-with-inwoix" element={<Growth />} />
+            <Route path="/CodetoCareer" element={<Hackathon />} />
+            <Route path="/admin" element={<Admin />} />
+            <Route path="/auth" element={<Auth />} />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
       </EngagementOrchestrator>
     </TooltipProvider>
   </QueryClientProvider>
